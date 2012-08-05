480ada5ec58d9a505614d8dfa77ce2853df6774d
Dan Dela Rosa <dandelarosa@gmail.com>
2012-08-05 13:34:52 -0400

    Basic collision detection

--- java/net/dandelarosa/pzdavid/SampleWorld.java
+++ java/net/dandelarosa/pzdavid/SampleWorld.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyEvent;
 public class SampleWorld implements World {
   private Input input;
   private Player player = new Player();
+  private Floor floor = new Floor();
   
   public SampleWorld(Input input) {
     this.input = input;
@@ -26,21 +27,61 @@ public class SampleWorld implements World {
     if(input.isDown(KeyEvent.VK_DOWN)) {
       player.y += 5;
     }
+    if(isCollision(player, floor)) {
+      System.out.println("Hello World!");
+    }
   }
 
   @Override
   public void paint(Graphics g) {
     player.paint(g);
+    floor.paint(g);
   }
   
-  private static class Player {
-    float x = 0;
-    float y = 0;
-    float width = 32;
-    float height = 32;
+  private boolean isCollision(Entity first, Entity second) {
+    if (first.x + first.width < second.x) {
+      return false;
+    }
+    else if (first.x > second.x + second.width) {
+      return false;
+    }
+    else if (first.y + first.height < second.y) {
+      return false;
+    }
+    else if (first.y > second.y + second.height) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  
+  private static abstract class Entity {
+    float x;
+    float y;
+    float width;
+    float height;
     public void paint(Graphics g) {
       g.setColor(Color.BLACK);
       g.drawRect((int)x, (int)y, (int)width, (int)height);
+    }
+  }
+  
+  private static class Player extends Entity {
+    public Player() {
+      x = 0;
+      y = 0;
+      width = 32;
+      height = 32;
+    }
+  }
+  
+  private static class Floor extends Entity {
+    public Floor() {
+      x = 0;
+      y = 336;
+      width = 549;
+      height = 32;
     }
   }
 }
